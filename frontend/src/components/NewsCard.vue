7f3e8d59ee1ed3021090ef34254534341faf12f0
Gustavo Tomás <guga.tomas10@gmail.com>
2022-08-29 16:18:02 -0300

    feature: adicionando cards de noticia e barra de pesquisa

---
+++ frontend/src/components/NewsCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <p>{{ msg }}</p>
+    <p id="date">{{ date }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NewsCard",
+    props: {
+      title: String,
+      msg: String,
+      date: String
+    },
+  };
+</script>
+
+<style scoped>
+  .card {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    border-radius: 5px;
+    background-color: var(--background-color-secondary);
+    padding: 45px 35px;
+  }
+  .card:hover {
+    color: green; /* apenas para visualizacao */
+  }
+  #date {
+    font-style: italic;
+  }
+</style>
