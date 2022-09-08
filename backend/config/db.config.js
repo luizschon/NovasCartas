ffb7951dfbe1199c64834b7c500d711001f4f3d2
rafaelnogalha <rafaelnogalha@gmail.com>
2022-09-08 00:05:39 -0300

    feature: Criação do endpoint para criação do usuário

---
+++ backend/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://dbUser:dbUser@novascartas.1dgcv37.mongodb.net/?retryWrites=true&w=majority"
+};
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ const cors = require('cors');
 const NewsRouter = require('./routes/news.js');
 
 const app = express()
+const db = require('./models/index.js')
+require('./routes/user.routes')(app);
+
+db.mongoose.connect(db.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Conectado ao Banco de Dados!");
+}).catch(err => {
+  console.log("Não foi possível conectar ao Banco de Dados!", err);
+  process.exit();
+})
 
 app.use(cors())
 
