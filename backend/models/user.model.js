a785aba3614d73ca9e9050d6e4565790bf6d1caa
Luiz Schonarth <luizcschonarth@gmail.com>
2022-09-16 19:23:56 -0300

    hotfix: Conserta duplicação de usuários (mongodb moment)

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const jwt = require("jsonwebtoken");
 
 const UserSchema = mongoose.Schema({
-    name: {type: String, required: true, unique: true},
+  name: {type: String, required: true, unique: true, dropDups: true},
     password: {type: String, required: true},
     tokens: [
       {
